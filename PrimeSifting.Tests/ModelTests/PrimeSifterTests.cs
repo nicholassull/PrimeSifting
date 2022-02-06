a46998f85a363f58c3a99b79dc191abdd43aea42
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 14:57:08 -0800

    add initial file structure

---
+++ PrimeSifting.Tests/ModelTests/PrimeSifterTests.cs
@@ -0,0 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PrimeSifter.Models;
+
+namespace PrimeSifter.TestTools
+{
+
+}
---
+++ PrimeSifting/Models/PrimeSifting.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+
+namespace PrimeSifter.Models
+{
+  
+}
