c2265f9cea4d3c9c2bcb313f6a4572c4a5d4efba
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 15:47:13 -0800

    add CreateList() and tests

--- PrimeSifting.Tests/ModelTests/PrimeSifterTests.cs
+++ PrimeSifting.Tests/ModelTests/PrimeSifterTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System;
 using PrimeSifter.Models;
 
@@ -12,6 +13,15 @@ namespace PrimeSifter.TestTools
     {
       Sifter newSifter = new Sifter(12);
       Assert.AreEqual(typeof(Sifter), newSifter.GetType());
+    }
+    [TestMethod]
+    public void CreateLists_CreatesLists_IntList()
+    {
+      Sifter newSifter = new Sifter(4);
+      List<int> testList = new List<int> {2, 3, 4};
+
+      List<int> result = newSifter.List01;
+      CollectionAssert.AreEqual(testList, result);
     }
   }
 }
--- PrimeSifting/Models/PrimeSifting.cs
+++ PrimeSifting/Models/PrimeSifting.cs
@@ -5,16 +5,25 @@ namespace PrimeSifter.Models
   public class Sifter
   {
     public int UserNumber { get; set; }
+    public List<int> List01 { get; set; }
+    public List<int> List02 { get; set; }
 
     public Sifter(int userNumber)
     {
       UserNumber = userNumber;
+      List01 = CreateLists();
+      List02 = CreateLists();
     }
 
-    // public List<int> CreateLists()
-    // {
-
-    // }
+    public List<int> CreateLists()
+    {
+      List<int> returnList = new List<int> {};
+      for (int i = 2; i <= UserNumber; i++)
+      {
+        returnList.Add(i);
+      }
+      return returnList;
+    }
     // public List<int> FindPrimes()
     // {
 
