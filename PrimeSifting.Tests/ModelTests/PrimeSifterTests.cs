0668d9abbba78538b954aed5bce7cf9012aa62ce
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 15:32:47 -0800

    added constructor test

--- PrimeSifting.Tests/ModelTests/PrimeSifterTests.cs
+++ PrimeSifting.Tests/ModelTests/PrimeSifterTests.cs
@@ -4,5 +4,14 @@ using PrimeSifter.Models;
 
 namespace PrimeSifter.TestTools
 {
-
+  [TestClass]
+  public class SifterTests
+  {
+    [TestMethod]
+    public void SifterConstructor_CreateInstanceOfSifter_Sifter()
+    {
+      Sifter newSifter = new Sifter(12);
+      Assert.AreEqual(typeof(Sifter), newSifter.GetType());
+    }
+  }
 }
--- PrimeSifting/Models/PrimeSifting.cs
+++ PrimeSifting/Models/PrimeSifting.cs
@@ -2,8 +2,22 @@ using System.Collections.Generic;
 
 namespace PrimeSifter.Models
 {
-  public class PrimeSifter
+  public class Sifter
   {
-    public 
+    public int UserNumber { get; set; }
+
+    public Sifter(int userNumber)
+    {
+      UserNumber = userNumber;
+    }
+
+    // public List<int> CreateLists()
+    // {
+
+    // }
+    // public List<int> FindPrimes()
+    // {
+
+    // }
   }
 }
