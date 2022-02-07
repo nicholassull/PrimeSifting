4effb64a8a4fb1618211bff671a48f1e09fdc3dc
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 16:49:37 -0800

    add a message if user inputs number out of range

--- PrimeSifting/Program.cs
+++ PrimeSifting/Program.cs
@@ -12,13 +12,21 @@ namespace PrimeSifter
       Console.WriteLine("=========================");
       Console.WriteLine("Enter a number between 2 and 100 and I will tell you all the primes found before it.");
       int userNumber = int.Parse(Console.ReadLine());
-      Sifter newSifter = new Sifter(userNumber);
-      List<int> primeList = newSifter.FindPrimes();
-      Console.WriteLine("=========================");
-      Console.WriteLine("The primes between 2 and " + userNumber.ToString() + ":");
-      foreach (int number in primeList)
+      if (userNumber < 2 || userNumber > 100)
       {
-        Console.WriteLine(number);
+        Console.WriteLine("That's not a number between 2 and 100. Please try again.");
+        Main();
+      } 
+      else
+      {
+        Sifter newSifter = new Sifter(userNumber);
+        List<int> primeList = newSifter.FindPrimes();
+        Console.WriteLine("=========================");
+        Console.WriteLine("The primes between 2 and " + userNumber.ToString() + ":");
+        foreach (int number in primeList)
+        {
+          Console.WriteLine(number);
+        }
       }
     }
 
