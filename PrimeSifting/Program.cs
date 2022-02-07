c933a2135c38b4955fcd8b3a51df75a6f29fe65b
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 16:38:05 -0800

    add initial terminal UI

--- PrimeSifting/Program.cs
+++ PrimeSifting/Program.cs
@@ -7,7 +7,10 @@ namespace PrimeSifter
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to the Prime Sifter!");
+      Console.WriteLine("=========================");
+      Console.WriteLine("Enter a number between 2 and 100 and I will tell you all the primes found before it.");
+      int userNumber = int.Parse(Console.ReadLine());
     }
 
   }
