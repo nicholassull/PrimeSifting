5648889d4ded411b2e15e5ed9246b1b3a68abdec
Nick Sullivan <nicholassull@gmail.com>
2022-02-06 16:44:42 -0800

    add UI functionality

--- PrimeSifting/Program.cs
+++ PrimeSifting/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PrimeSifter.Models;
 
 namespace PrimeSifter
 {
@@ -11,6 +12,14 @@ namespace PrimeSifter
       Console.WriteLine("=========================");
       Console.WriteLine("Enter a number between 2 and 100 and I will tell you all the primes found before it.");
       int userNumber = int.Parse(Console.ReadLine());
+      Sifter newSifter = new Sifter(userNumber);
+      List<int> primeList = newSifter.FindPrimes();
+      Console.WriteLine("=========================");
+      Console.WriteLine("The primes between 2 and " + userNumber.ToString() + ":");
+      foreach (int number in primeList)
+      {
+        Console.WriteLine(number);
+      }
     }
 
   }
